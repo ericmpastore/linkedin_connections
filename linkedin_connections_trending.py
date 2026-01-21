dca593f883cf7c7a6bdb09ed65e738f7754996b3
Eric P <epas001@hotmail.com>
2026-01-20 23:42:24 -0500

    Added dataframe to start analyzing data

--- linkedin_connections_trending.py
+++ linkedin_connections_trending.py
@@ -1,7 +1,13 @@
+import numpy as np
 import pandas as pd
+import matplotlib as plt
 
-def main():
-    print("Let's make a dataframe!")
+df = pd.read_csv('Connections.csv',encoding='cp1252')
 
-if __name__ == "__main__":
-    main()
+print(df.head(20))
+
+# def main():
+#     print("Let's make a dataframe!")
+
+# if __name__ == "__main__":
+#     main()
