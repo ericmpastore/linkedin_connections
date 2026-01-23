e1cccdbdd9b9f18b96ab784b8720152e62431387
Eric P <epas001@hotmail.com>
2026-01-23 12:58:31 -0500

    Finished and tested render_plot function

--- linkedin_trends.py
+++ linkedin_trends.py
@@ -3,8 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
-def render_plot():
-    print("Plot is rendered.")
+def render_plot(csv_file='Connections.csv'):
+    """
+    Docstring for render_plot
+
+    Render plot reads data from csv file, parses, and shows plot
+    
+    :param csv_file: Parameter identifies file to read source data
+    """
+    
+    connections_count = pd.read_csv(csv_file,encoding='cp1252')
+
+    connections_count['Connected On'] = pd.to_datetime(connections_count['Connected On'])
+    cutoff_date = datetime.now() - timedelta(days=365)
+    connections_count = connections_count[connections_count['Connected On'] >= cutoff_date]
+
+    connections_count['connection_count']=1
+
+    final_frame = connections_count.groupby(pd.Grouper(key='Connected On', freq='MS'))['connection_count'].sum()
+
+    final_frame.plot(kind='line')
+    plt.xlabel('Month')
+    plt.ylabel('Connection Count')
+    plt.title('LinkedIn Connections per Month (Last 365 Days)')
+    plt.show()
+
+    
 
 def main():
     render_plot()
